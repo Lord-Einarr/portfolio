51168fa26627cf0457c8ae96ca65e1ba31abcd94
Lord-Einarr <lord.einarr@gmail.com>
2017-03-20 15:47:20 +1300

    instructions one complete
    completed the first set of instructions

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+ require 'includes/functions.php';
+
+$host = 'localhost';
+$database = 'portfolio';
+$user = 'root';
+$pass = '';
+
+$dbh = connectDatabase($host, $database, $user, $pass);
---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+    </div><!-- DIV ID=APP -->
+    <!-- Scripts -->
+    <script src="js/app.js"></script>
+    <script type="text/javascript" src=js/main.js></script>
+</body>
+</html>
